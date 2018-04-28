cdae7f04abc6a8d922b76674f0eb5a223371e8f3
Soorya Annadurai <soorya.annadurai@gmail.com>
2018-04-28 14:01:42 +0530

    Made email script demonstratable.

--- DesktopEmailServer.py
+++ DesktopEmailServer.py
@@ -1,14 +1,21 @@
-import time
-import serial
+# import time
+# import serial
 import smtplib
 
 
-TO = 'putyour@email.here'
-GMAIL_USER = 'putyour@email.here'
-GMAIL_PASS = 'putyourpasswordhere'
-SUBJECT = 'Intrusion!!'
-TEXT = 'Your PIR sensor detected movement'
-ser = serial.Serial('COM4', 9600)
+# TO = 'putyour@email.here'
+# GMAIL_USER = 'putyour@email.here'
+# GMAIL_PASS = 'putyourpasswordhere'
+# SUBJECT = 'Intrusion!!'
+# TEXT = 'Your PIR sensor detected movement'
+
+
+TO = 'soorya.annadurai@gmail.com'
+GMAIL_USER = 'missile.blast@gmail.com'
+GMAIL_PASS = 'Cannonballs13'
+SUBJECT = 'Test subject'
+TEXT = 'Sample text in the email. Cheers!'
+# ser = serial.Serial('COM4', 9600)
 
 
 def send_email():
@@ -25,9 +32,10 @@ def send_email():
 	smtpserver.sendmail(GMAIL_USER, TO, msg)
 	smtpserver.close()
 
-while True:
-	message = ser.readline()
-	print(message)
-	if message[0] == 'M' :
-	send_email()
-	time.sleep(0.5)
+# while True:
+# 	message = ser.readline()
+# 	print(message)
+# 	if message[0] == 'M' :
+# 		send_email()
+# 	time.sleep(0.5)
+send_email()
