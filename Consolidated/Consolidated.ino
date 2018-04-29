b5dd4cc21a762ff5be1d58958f800bffe24e9fe7
Bindhu Anil <bindhuanil@Bindhus-MacBook-Pro.local>
2018-04-29 20:21:58 +0530

    Consolidated pH SensorCode

--- Consolidated/Consolidated.ino
+++ Consolidated/Consolidated.ino
@@ -1,14 +1,38 @@
 #include <DallasTemperature.h> //For Liquid Temperature Sensor
+
 #include <OneWire.h> // For Liquid Temperature Sensor
+
 #include <dht.h> // For DHT11 Temperature Sensor 
+
 #define LIQUIDTEMP_ONE_WIRE_BUS 2 // Digital Pin 2 for Liquid Temperature Sensor 
+
 #define DHT11Temp_PIN 7 // Digital Pin 7 for DHT11 Temperature Sensor
+
+#define pHSensor_Pin A2 //pH meter Analog output to Arduino Analog Input Pin 2 for pH Sensor
+
+#define pHSensor_Offset 0.00 //Deviation compensate for pH Sensor
+
+#define pHSensor_LED 13 // For pH sensor 
+
+#define pHSensor_samplingInterval 20 // For pH Sensor
+
+#define pHSensor_printInterval 800 //For pH Sensor
+
+#define pHSensor_ArrayLenth  40 //Times of collection for pH Sensor
+
 OneWire LiquidTemp_oneWire(LIQUIDTEMP_ONE_WIRE_BUS); //For Liquid Temperature Sensor
+
 DallasTemperature LiquidTemp_sensors(&LiquidTemp_oneWire); //For Liquid Temperature Sensor
+
 dht DHT11Temp_DHT; //For DHT11 Temperature Sensor
+
 int soil_moisture_sensor_pin = A0; // For Soil Moisture Sensor
+
 int ThermistorTemp_ThermistorPin = A1; //Thermistor Temperature Analog Pin
 
+int pHSensor_pHArray[pHSensor_ArrayLenth]; //Store the average value of the sensor feedback for pH Sensor
+
+int pHSensor_pHArrayIndex=0; // For pH Sensor 
 
 void setup() {
 
@@ -17,6 +41,7 @@ void setup() {
   SoilMoistureSetup(); // For Soil Moisture Sensor
   ThermistorTemp_Setup(); // For Thermistor Temperature Sensor
   DHT11Temp_Setup(); // For DHT11 Temperature Sensor
+  phSensor_Setup();
 
    }
 
@@ -50,6 +75,12 @@ void loop() {
   
   Serial.println(DHT11_Output); // For DHT11 Temperature Sensor 
    
+  float pHSensor_Output = pHSensor_Loop();
+  
+  Serial.print("pH value: ");
+  
+  Serial.print(pHSensor_Output);
+  
   delay(1000);
    
    }
@@ -142,4 +173,72 @@ int DHT11Temp_Loop()
   int DHT11Temp_Output = DHT11Temp_DHT.temperature;
   return DHT11Temp_Output;
 }
+
+void phSensor_Setup()
+{
+  pinMode(pHSensor_LED,OUTPUT);  
+  Serial.println("pH meter experiment!");    //Test the serial monitor
+}
+
+float pHSensor_Loop()
+{
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue,voltage;
+  if(millis()-samplingTime > pHSensor_samplingInterval)
+  {
+      pHSensor_pHArray[pHSensor_pHArrayIndex++]=analogRead(pHSensor_Pin);
+      if(pHSensor_pHArrayIndex==pHSensor_ArrayLenth)pHSensor_pHArrayIndex=0;
+      voltage = avergearray(pHSensor_pHArray, pHSensor_ArrayLenth)*5.0/1024;
+      pHValue = 3.5*voltage+pHSensor_Offset;
+      samplingTime=millis();
+  }
+  if(millis() - printTime > pHSensor_printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  {
+        digitalWrite(pHSensor_LED,digitalRead(pHSensor_LED)^1);
+        printTime=millis();
+        return pHValue;
+  }   
+}  
+
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
+}
+
 
